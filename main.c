e7f27931aa20a07d2b156f49f20d8764baafa04d
vincentledoux <vincen.ledoux@gmail.com>
2016-12-02 08:55:02 +0100

    Beginning of the Open and read
    open and read the csv, start

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAILLE_MAX 10000
+
+
+
+void openAndRead(char fileName)
+{
+      FILE* myFile = NULL
+      char chaine[TAILLE_MAX];
+      myFile = fopen(fileName);
+
+      if(myFile != NULL){
+            while(fgets(chaine, TAILLE_MAX, fichier) != NULL)
+            {
+
+            }
+
+      }
+}
+
 
 int main()
 {
